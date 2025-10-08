21c17b26ed3b77fc36490b3fc141c230c176bd2c
william-leke <william.leke22@gmail.com>
2025-10-08 11:36:34 -0500

    code revision

--- Lambda/Lambda_function.py
+++ Lambda/Lambda_function.py
@@ -3,5 +3,5 @@ import json
 def lambda_handler(event, context):
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from CICD github actions worfklow vscode!')
+        'body': json.dumps('Hello from updated Lambda function VScode!')
     }
