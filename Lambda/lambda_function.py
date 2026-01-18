49b84ebe014a87453840dbc4be263c59530c528f
William Leke <william.leke22@gmail.com>
2026-01-17 22:01:54 -0600

    updates  to lambda function

--- Lambda/lambda_function.py
+++ Lambda/lambda_function.py
@@ -3,5 +3,5 @@ import json
 def lambda_handler(event, context):
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from our github actions vscode!')
+        'body': json.dumps('Hello from our github actions vscode cicd pipeline!')
     }
