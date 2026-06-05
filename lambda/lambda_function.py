62738af6462c5dd1b93cc1da3d0d497eed808395
William Leke <william.leke22@gmail.com>
2026-06-05 15:39:13 -0500

    initial setup of lambda

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -2,6 +2,6 @@ import json
 
 def lambda_handler(event, context):
     return {
-        'statuscode':200,
-        'body': json.dumps('Hello from Williams Lambda on 6/6!')
+        'statusCode': 200,
+        'body': json.dumps('Hello from CICD Lambda!')
     }
