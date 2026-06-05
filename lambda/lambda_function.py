0e38e57af81f1dac8e2b63e0087198e3c48998ee
William Leke <william.leke22@gmail.com>
2026-06-05 15:32:24 -0500

    initial setup

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -3,5 +3,5 @@ import json
 def lambda_handler(event, context):
     return {
         'statuscode':200,
-        'body': json.dumps('Hello from Williams Lambda on 6/6/2025!')
+        'body': json.dumps('Hello from Williams Lambda on 6/6!')
     }
